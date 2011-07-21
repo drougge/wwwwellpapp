9e5822975f842fa74fbc844b6d1ce860ef39b096
Carl Drougge <bearded@longhaired.org>
2011-07-21 18:08:21 +0200

    Filesystem based image browsing. (Inefficient.)
    This is intended for tagging stuff you have already sorted into dirs.
    Not really useful yet.

---
+++ cgi/browse.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import environ, listdir, stat
+from sys import exit
+from common import *
+from os.path import normpath, exists, join as joinpath, isfile, isdir
+from hashlib import md5
+
+def forbidden():
+	global outdata
+	print "Status: 403 Forbidden"
+	outdata = []
+	prt_head()
+	prt(u'<h1>Forbidden</h1>\n')
+	prt(u'<p>You are not allowed to access this directory.</p>\n')
+	prt_foot()
+	finish()
+	exit(0)
+
+def md5file(fn):
+	m = md5()
+	with file(fn, "rb") as fh:
+		for data in iter(lambda: fh.read(65536), ""):
+			m.update(data)
+	return m.hexdigest()
+
+if not client.cfg.browsebase: forbidden()
+path = normpath(client.cfg.browsebase + normpath("/" + environ["PATH_INFO"]))
+if not exists(path): forbidden()
+
+posts = []
+dirs = []
+for fn in listdir(path):
+	ffn = joinpath(path, fn)
+	if isfile(ffn):
+		m = md5file(ffn)
+		p = client.get_post(m)
+		if p:
+			t = stat(ffn).st_mtime
+			posts.append((t, p))
+	elif isdir(ffn):
+		dirs.append(fn)
+dirs.sort()
+if path != client.cfg.browsebase: dirs = [".."] + dirs
+
+prt_head()
+prt(u'<div id="left">\n')
+prt(u'<ul id="dirs">\n')
+for d in dirs:
+	prt(u'<li><a href="' + d + u'/">' + d + u'</a></li>\n')
+prt(u'</ul>\n')
+prt(u'</div>\n')
+prt(u'<div id="main">\n')
+prt_posts([p[1] for p in sorted(posts)])
+prt(u'</div>\n')
+prt_foot()
+finish()
