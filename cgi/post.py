025c755c4998177980b2b7cbd189b23c5f695071
Carl Drougge <bearded@longhaired.org>
2011-07-27 05:44:31 +0200

    Try to make image rotation work in more browsers.
    I use Opera. The old version worked in Opera. Looking at the standards, it
    should work in everything with SVG support. So of course it worked in
    nothing except Opera. Using <object> makes me a little sad, but it does
    allow for showing a message and unrotated image in browsers without SVG
    support. Plus it works in most browsers, that's a plus too.

--- cgi/post.py
+++ cgi/post.py
@@ -16,21 +16,34 @@ if not post: notfound()
 
 tags = taglist(post, False) + taglist(post, True)
 rels = client.post_rels(m)
-if post.rotate > 0:
-	spec = u'%(width)dx%(height)d-%(rotate)d' % post
-	img = base + u'rotate/' + spec + u'/' + m + u'.' + post.ext
-else:
-	img = base + u'image/' + m + u'.' + post.ext
 
 prt_head(u'resize.js')
 prt(u'<div id="main">\n',
     u'<noscript><div id="no-resize" class="msgbox">',
     u'If you had javascript, image resizing might work',
     u'</div></noscript>\n',
-    u'<div onclick="return WP.size.toggle(false);" id="rescaled-msg" class="msgbox"></div>\n',
-    u'<img id="main-image" onclick="return WP.size.toggle(false);" ')
-prtfields((u'src', img), (u'alt', m), (u'width', post.width), (u'height', post.height))
-prt(u'/>\n')
+    u'<div onclick="return WP.size.toggle(false);" id="rescaled-msg" class="msgbox"></div>\n')
+
+def prt_img(m, post, id=u'main-image'):
+	img = base + u'image/' + m + u'.' + post.ext
+	prt(u'<img onmousedown="return WP.size.toggle(false);" ')
+	prtfields((u'src', img), (u'alt', m), (u'id', id),
+	          (u'width', post.width), (u'height', post.height))
+	prt(u'/>\n')
+
+if post.rotate > 0:
+	spec = u'%(width)dx%(height)d-%(rotate)d' % post
+	svg = base + u'rotate/' + spec + u'/' + m + u'.' + post.ext
+	prt(u'<object type="image/svg+xml" id="main-image" ')
+	prtfields((u'data', svg), (u'width', post.width), (u'height', post.height))
+	prt(u'>\n')
+	if post.rotate in (90, 270):
+		post.width, post.height = post.height, post.width
+	prt(u' <div>This image should be rotated, but your browser does not appear to support that.</div>\n ')
+	prt_img(m, post, "fallback-image")
+	prt(u'</object>\n')
+else:
+	prt_img(m, post)
 prt(u"""<script type="text/javascript">
 <!--
 WP.size.toggle(true);
--- cgi/rotate.py
+++ cgi/rotate.py
@@ -32,8 +32,13 @@ if not re.match(r"^[0-9a-f]{32}$", imga[0]): notfound()
 
 data = """<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="%(sw)d" height="%(sh)d" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<image width="%(iw)d" height="%(ih)d" y="%(y)d" x="%(x)d" xlink:href="%(url)s" transform="rotate(%(rot)s)"/>
+<svg width="100%%" height="100%%" version="1.1"
+ viewBox="0 0 %(sw)d %(sh)d"
+ preserveAspectRatio="xMinYMin meet"
+ xmlns="http://www.w3.org/2000/svg"
+ xmlns:xlink="http://www.w3.org/1999/xlink">
+<image width="%(iw)d" height="%(ih)d" y="%(y)d" x="%(x)d"
+ xlink:href="%(url)s" transform="rotate(%(rot)s)"/>
 </svg>"""
 
 if rot == "90":
