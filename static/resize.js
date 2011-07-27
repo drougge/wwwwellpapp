a528dfc01301d210f89c50e9b8ad797a7b08aa2a
Carl Drougge <bearded@longhaired.org>
2011-07-27 03:59:40 +0200

    Scale image to fit both width and height. (Back to two states.)

--- static/resize.js
+++ static/resize.js
@@ -1,7 +1,7 @@
-/* Toggle the image between full size, and fitting window width.               *
+/* Toggle the image between full size, and fitting in the window.              *
  * MarginWidth is to make room both for the left div and a possible scrollbar. *
  * MarginHeight is to make room for a possible scrollbar.                      *
- * State is 1 for original size, 2 for fit to width and 3 for fit to height.   *
+ * State is 1 for full size, 2 for rescaled. (Or undefined before init.)       *
  * The message is only displayed if resizing is triggered automatically.       *
  */
 
@@ -14,13 +14,15 @@
 		WP.size.msg = document.createElement("div");
 		WP.size.msg.appendChild(document.createTextNode("Image rescaled"));
 		WP.size.msg.appendChild(document.createElement("br"));
-		WP.size.msg.appendChild(document.createTextNode("click to change size"));
+		WP.size.msg.appendChild(document.createTextNode("click to see full size"));
 		WP.size.div.appendChild(WP.size.msg);
 		WP.size.div.style.display = "block";
 	};
 
 	WP.size.toggle = function (show) {
-		var iw, ih, ww, wh, did = false;
+		var ww, wh;   // Window size
+		var iw, ih;   // Rescaled image size
+		var hih;      // Temp for checking height scaling
 		if (document.body && document.body.offsetWidth) {
 			ww = document.body.offsetWidth;
 			wh = document.body.offsetHeight;
@@ -29,6 +31,7 @@
 			ww = window.innerWidth;
 			wh = window.innerHeight;
 		}
+
 		if (!WP.size.state) {
 			WP.size.img = document.getElementById("main-image");
 			WP.size.div = document.getElementById("rescaled-msg");
@@ -41,32 +44,31 @@
 			WP.size.div.removeChild(WP.size.msg);
 			WP.size.msg = null;
 		}
-		if (WP.size.state === 1) {
-			iw = ww - WP.size.marginWidth;
-			if (iw < 128) { iw = 128; }
-			if (iw < WP.size.orgWidth) {
-				WP.size.img.width = iw;
-				WP.size.img.height = iw * WP.size.orgHeight / WP.size.orgWidth;
-				did = true;
-			}
-			WP.size.state = 2;
+
+		iw = Math.max(ww - WP.size.marginWidth, 128);
+		if (iw < WP.size.orgWidth) {
+			ih = iw * WP.size.orgHeight / WP.size.orgWidth;
+		} else {
+			iw = WP.size.orgWidth;
+			ih = WP.size.orgHeight;
 		}
-		if (!did && WP.size.state === 2) {
-			ih = wh - WP.size.marginHeight;
-			if (ih < 128) { ih = 128; }
-			if (ih < WP.size.orgHeight) {
-				WP.size.img.height = ih;
-				WP.size.img.width = ih * WP.size.orgWidth / WP.size.orgHeight;
-				did = true;
-			}
-			WP.size.state = 3;
+		hih = Math.max(wh - WP.size.marginHeight, 128);
+		if (hih < WP.size.orgHeight && hih < ih) {
+			ih = hih;
+			iw = ih * WP.size.orgWidth / WP.size.orgHeight;
 		}
-		if (!did) {
+
+		if (WP.size.state === 2 || iw === WP.size.orgWidth) {
+			// We want full size or image fits as is.
 			WP.size.img.width = WP.size.orgWidth;
 			WP.size.img.height = WP.size.orgHeight;
 			WP.size.state = 1;
+		} else {
+			WP.size.img.width = iw;
+			WP.size.img.height = ih;
+			WP.size.state = 2;
+			if (show) { WP.size.show_msg(); }
 		}
-		if (did && show) { WP.size.show_msg(); }
 		return false;
 	};
 }());
